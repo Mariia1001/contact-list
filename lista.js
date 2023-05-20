e69f4856702e81f2de24836b2ed96bd8fb20d685
Mariia1001 <m.alejamonsalve@hotmail.com>
2023-05-20 13:23:12 -0500

    agrego boton update

--- lista.js
+++ lista.js
@@ -33,7 +33,8 @@ var count=0;
 			var td5 = document.createElement("TD");
 			td5.appendChild(document.createTextNode(direccion));
 			var td6 = document.createElement("TD");
-			td6.appendChild(crearButton(count));
+			td6.appendChild(crearButton(count, "Eliminar"));
+			td6.appendChild(crearButton(count, "update"));
  
 			var row = document.createElement("TR");
 			row.setAttributeNode(attrId);
@@ -62,20 +63,54 @@ var count=0;
 		var row = document.getElementById(id);
 		row.parentNode.removeChild(row);
 	}
+	//funcion pra actulizar valor
+	function actualizarValor(id){
+		var row = document.getElementById(id);
+	
+		var nombre=document.getElementById("nombre").value;
+		var apellido=document.getElementById("apellido").value;
+		var correo=document.getElementById("correo").value;
+		var telefono=document.getElementById("telefono").value;
+		var direccion=document.getElementById("direccion").value;
+	
+		if ( nombre.length>0 || apellido.length>0) {
+			row.childNodes[0].textContent = nombre;
+			row.childNodes[1].textContent = apellido;
+			row.childNodes[2].textContent = correo;
+			row.childNodes[3].textContent = telefono;
+			row.childNodes[4].textContent = direccion;
+		}
+	
+		document.getElementById("nombre").value = "";
+		document.getElementById("apellido").value = "";
+		document.getElementById("correo").value = "";
+		document.getElementById("telefono").value = "";
+		document.getElementById("direccion").value = "";
+	}
+	
  
 	/**
 	 * Función que crea el boton de eliminar
 	 * Tiene que recibie el id a eliminar
 	 */
-	function crearButton(id)
+	function crearButton(id, type)
 	{
 		var button=document.createElement("input");
 		var attrType=document.createAttribute('type');
 		attrType.value="button";
-		var attrValue=document.createAttribute('value');
-		attrValue.value="Eliminar";
-		var attrOnclick=document.createAttribute('onclick');
-		attrOnclick.value="eliminarValor("+count+");";
+		if (type == "Eliminar") {
+			var attrValue=document.createAttribute('value');
+			attrValue.value="Eliminar";
+			var attrOnclick=document.createAttribute('onclick');
+			attrOnclick.value="eliminarValor("+count+");";
+		}else {
+			var attrValue=document.createAttribute('value');
+			attrValue.value="update";
+			var attrOnclick=document.createAttribute('onclick');
+			attrOnclick.value="actualizarValor("+count+");";
+		}
+
+
  
 		button.setAttributeNode(attrType);
 		button.setAttributeNode(attrValue);
